513fa525a5be4a0431e412dcef9cf49a67c236f6
Aline Alves de Souza <alineconfidence@gmail.com>
2025-07-04 00:00:15 -0300

    header and main started

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+      },
+      spacing: {
+        big: '48rem',
+      },
+    },
+    colors: {
+      mycolor: '#555',
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans - serif'],
+    },
+  },
+  plugins: [],
+};
